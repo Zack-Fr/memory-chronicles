6210da8130363ab7f3e4f6e2f8f9a9952b2b77e1
Zack-Fr <59012974+Zack-Fr@users.noreply.github.com>
2025-07-20 03:42:04 +0300

    Finished the UserAuth api's, with one pending bug related to frontend denied access to the server

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-Route::prefix('v1')->group(function () {
+Route::group(['prefix'=>'v1'], function () {
+    Route::group(["middleware" => "auth:api"], function () {
     Route::post('auth/register', [AuthController::class, 'register']);
     Route::post('auth/login',    [AuthController::class, 'login']);
-
-    Route::middleware('auth:api')->group(function () {
-        Route::post('auth/logout',  [AuthController::class, 'logout']);
-        Route::post('auth/refresh', [AuthController::class, 'refresh']);
-        // other protected routes...
-    });
+    
+    // other protected routes...
+});
+    Route::post('auth/logout',  [AuthController::class, 'logout']);
+    Route::post('auth/refresh', [AuthController::class, 'refresh']);
 });
